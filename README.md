d53ae5b27c759a5f5d2383714e0d9a36f06cc9c7
bsao17 <declic62@gmail.com>
2022-03-24 11:21:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-9ffbvt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-9ffbvt)
---
+++ index.js
@@ -0,0 +1,21 @@
+class Rotate {
+  constructor(nums, k) {
+    this.nums = nums;
+    this.k = k;
+  }
+  rotation() {
+    for (let i = 0; i < this.k; i++) {
+      let itemOne = this.nums.pop();
+      this.nums.splice(0, 0, itemOne);
+    }
+    return this.nums;
+  }
+}
+
+const firstRotate = new Rotate([1, 2, 3, 4, 5, 6, 7], 3);
+const secondRotate = new Rotate([-1, -100, 3, 99], 2);
+const thirdRotate = new Rotate([100, 25, -10, 250, 999], 4);
+
+console.log(firstRotate.rotation());
+console.log(secondRotate.rotation());
+console.log(thirdRotate.rotation());
